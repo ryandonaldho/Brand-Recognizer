cc2a61f10b2648ea031af4f72c117cb1d7b37671
Ryan Ho <ryandonaldho@gmail.com>
2017-10-23 20:08:18 -0700

    buttons and imageview

--- app/src/main/java/brand/brandrecognizer/MainActivity.java
+++ app/src/main/java/brand/brandrecognizer/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String CLOUD_VISION_API_KEY = "AIzaSyCKKR9KzqqTGUzzJysbOr_E2nKdPz-8q7M";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
