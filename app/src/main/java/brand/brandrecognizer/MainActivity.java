f8196e5fd8112304b2b6da7ce5ead1e47c3ac6b9
unknown <hesgill@ucsc.edu>
2017-11-01 17:24:33 -0700

    map added, needs to be more accurate compared to google.maps

--- app/src/main/java/brand/brandrecognizer/MainActivity.java
+++ app/src/main/java/brand/brandrecognizer/MainActivity.java
@@ -1,7 +1,9 @@
 package brand.brandrecognizer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void mapClick(View view){
+        Intent enterIntent = new Intent(MainActivity.this, MapsActivity.class);
+        startActivity(enterIntent);
+
     }
     // test
 }
