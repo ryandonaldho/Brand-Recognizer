2affdc35c04fc431642a5b3fdae33abd80d3a7cc
Ryan Ho <ryandonaldho@gmail.com>
2017-11-28 20:10:01 -0800

    working listview from firebase past searches temp using in place of map fragment on tab activity

---
+++ app/src/main/java/brand/brandrecognizer/UserInfo.java
@@ -0,0 +1,28 @@
+package brand.brandrecognizer;
+
+/**
+ * Created by Ryan on 11/27/2017.
+ */
+
+public class UserInfo {
+    String userName;
+    String email;
+
+    public String getUserName(){
+        return userName;
+    }
+
+    public void setUserName(String userName){
+        this.userName = userName;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+
+}
