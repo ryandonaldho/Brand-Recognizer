172cc8a75b07147d78d4ed30194d8b9a4541c1a9
unknown <hesgill@ucsc.edu>
2017-12-05 01:36:57 -0800

    map location and searching for the brand working

--- app/src/main/java/brand/brandrecognizer/MapFragment.java
+++ app/src/main/java/brand/brandrecognizer/MapFragment.java
@@ -7,83 +7,46 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-
 import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-/*
-public class MapFragment extends Fragment {
-
-
-    WebView webView;
-
-    public MapFragment() {
-        // Required empty public constructor
-    }
-
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_map, container, false);
-
-
-        return v;
-    }
-
-}*/
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 
 /**
  * Created by Hersh on 11/27/2017.
  */
 
-public class MapFragment extends Fragment implements OnMapReadyCallback,
-        GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener,
-        LocationListener {
-
+public class   MapFragment extends Fragment implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        /*//Toast.makeText(this, "map is ready", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "map is ready", Toast.LENGTH_SHORT).show();
         Log.d(TAG, "onMapReady: map is ready");
         mMap = googleMap;
 
         if (mLocationPermissionsGranted) {
-            //getDeviceLocation();
+            getDeviceLocation();
 
             if (ContextCompat.checkSelfPermission(this.getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 //
@@ -95,18 +58,10 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
                 // for ActivityCompat#requestPermissions for more details.
                 return;
             }
-            Log.d(TAG, "set my location");
             mMap.setMyLocationEnabled(true);
-            mUiSettings.setMyLocationButtonEnabled(true);
             //init();
             //use mMap.getUiSettings().
             //for different options.
-        }*/
-        mMap = googleMap;
-
-        if (ContextCompat.checkSelfPermission(this.getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            bulidGoogleApiClient();
-            mMap.setMyLocationEnabled(true);
         }
     }
 
@@ -122,135 +77,47 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
     private Boolean mLocationPermissionsGranted = false;
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationPRoviderClient;
+
     private GoogleApiClient client;
     private LocationRequest locationRequest;
     private Location lastlocation;
     private Marker currentLocationmMarker;
     public static final int REQUEST_LOCATION_CODE = 99;
     int PROXIMITY_RADIUS = 10000;
-    double latitude, longitude;
+    double latitude,longitude;
+
+
+    Object dataTransfer[] = new Object[2];
+    GetNearbyPlacesData getNearbyPlacesData = new GetNearbyPlacesData();
     String brand;
 
 
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_map);
         //setContentView(R.layout.fragment_map);
         View v = inflater.inflate(R.layout.fragment_map, container, false);
-        Object dataTransfer[] = new Object[2];
-        GetNearbyPlacesData getNearbyPlacesData = new GetNearbyPlacesData();
 
-        Bundle bundle = this.getArguments();
-        brand = bundle.getString("brand");
+        getLocationPermission();
 
-        String url = getUrl(latitude, longitude, brand);
-
-        dataTransfer[0] = mMap;
-        dataTransfer[1] = url;
-        //getNearbyPlacesData.execute(dataTransfer);
-        //getLocationPermission();
 
         return v;
         //init();
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_LOCATION_CODE:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    if (ContextCompat.checkSelfPermission(this.getActivity(), Manifest.permission.
-                            ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                        if (client == null) {
-                            bulidGoogleApiClient();
-                        }
-                        mMap.setMyLocationEnabled(true);
-                    }
-                } else {
-                    Toast.makeText(this.getActivity(), "Permission Denied", Toast.LENGTH_LONG).show();
-                }
-        }
+    private int checkPermission(String permission) {
+        return ContextCompat.checkSelfPermission(this.getActivity(), permission);
     }
 
-    protected synchronized void bulidGoogleApiClient() {
-        client = new GoogleApiClient.Builder(this.getActivity()).addConnectionCallbacks(this).addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
-        client.connect();
-
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-
-        latitude = location.getLatitude();
-        longitude = location.getLongitude();
-        lastlocation = location;
-        if (currentLocationmMarker != null) {
-            currentLocationmMarker.remove();
-
-        }
-        Log.d("lat = ", "" + latitude);
-        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(latLng);
-        markerOptions.title("Current Location");
-        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
-        currentLocationmMarker = mMap.addMarker(markerOptions);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-        mMap.animateCamera(CameraUpdateFactory.zoomBy(10));
-
-        if (client != null) {
-            LocationServices.FusedLocationApi.removeLocationUpdates(client, this);
-        }
-    }
-
-    private String getUrl(double latitude, double longitude, String nearbyPlace) {
-
-        StringBuilder googlePlaceUrl = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
-        googlePlaceUrl.append("location=" + latitude + "," + longitude);
-        googlePlaceUrl.append("&radius=" + PROXIMITY_RADIUS);
-        googlePlaceUrl.append("&type=stores");
-        googlePlaceUrl.append("&keyword=" + brand);
-        googlePlaceUrl.append("&key=" + "AIzaSyAQ0tHxeLZE3pElkuH5xiTl12lrBhMoSlw");
-
-        Log.d("MapsActivity", "url = " + googlePlaceUrl.toString());
-
-        return googlePlaceUrl.toString();
-    }
-
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-
-        /*locationRequest = new LocationRequest();
-        locationRequest.setInterval(100);
-        locationRequest.setFastestInterval(1000);
-        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
-        mFusedLocationPRoviderClient = LocationServices.getFusedLocationProviderClient(this.getActivity());
-
-
-        if(ContextCompat.checkSelfPermission(this.getActivity(), Manifest.permission.ACCESS_FINE_LOCATION ) == PackageManager.PERMISSION_GRANTED)
-        {
-            final Task location = mFusedLocationPRoviderClient.getLastLocation();
-            //LocationServices.FusedLocationApi.requestLocationUpdates(client, locationRequest, this);
-        }*/
-
+    private void getDeviceLocation(){
         Log.d(TAG, "getting the device current locaiton");
         mFusedLocationPRoviderClient = LocationServices.getFusedLocationProviderClient(this.getActivity());
-        try {
-            if (mLocationPermissionsGranted) {
+        try{
+            if(mLocationPermissionsGranted){
                 //error with getLastLocation returning NULL
                 //will work if another app is returning before hand using location services.
-                if (ActivityCompat.checkSelfPermission(this.getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
-                }
                 final Task location = mFusedLocationPRoviderClient.getLastLocation();
                 location.addOnCompleteListener(new OnCompleteListener() {
                     @Override
@@ -259,13 +126,21 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
                             Log.d(TAG, "onComplete: found location");
                             Location currentLocation = (Location) task.getResult();
 
-                            /*double latitude = 0;
-                            double longitude = 0;
+
                             if (currentLocation != null) {
                                 longitude = currentLocation.getLongitude();
                                 latitude = currentLocation.getLatitude();
-                                String locLat = String.valueOf(latitude) + "," + String.valueOf(longitude);
-                            }*/
+
+                                String url = getUrl(latitude, longitude, brand);
+
+                                dataTransfer[0] = mMap;
+                                dataTransfer[1] = url;
+
+                                System.out.println(url);
+
+                                getNearbyPlacesData.execute(dataTransfer);
+
+                            }
 
                             moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), DEFAULT_ZOOM);
 
@@ -276,44 +151,87 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
                     }
                 });
             }
+
         }catch (SecurityException e){
-        Log.e(TAG, "getDeviceLocation: SecurityException: " + e.getMessage());
-    }
+            Log.e(TAG, "getDeviceLocation: SecurityException: " + e.getMessage());
+        }
     }
 
-        private void moveCamera(LatLng latLng, float zoom){
-            Log.d(TAG, "moveCamera: moving camera to lat: " + latLng.latitude + ", lng: " + latLng.longitude);
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+    private void moveCamera(LatLng latLng, float zoom){
+        Log.d(TAG, "moveCamera: moving camera to lat: " + latLng.latitude + ", lng: " + latLng.longitude);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+
+    }
+    private void initMap(){
+        Log.d(TAG,"initMap: initialize map");
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(MapFragment.this);
+    }
+    private void getLocationPermission(){
+        Log.d(TAG, "getLocationPermissions: getting location permissions");
+        String[] permissions = {android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        if(ContextCompat.checkSelfPermission(this.getActivity(),
+                FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this.getActivity(),
+                    COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mLocationPermissionsGranted = true;
+                initMap();
+            } else {
+                ActivityCompat.requestPermissions(this.getActivity(), permissions, LOCATION_PERMISSION_REQUEST_CODE);
+
+            }
+        }else{
+            ActivityCompat.requestPermissions(this.getActivity(),
+                    permissions,LOCATION_PERMISSION_REQUEST_CODE);
 
         }
-    public boolean checkLocationPermission()
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.d(TAG, "onRequestPermissionsResults: called");
+        mLocationPermissionsGranted = false;
+
+        switch(requestCode){
+            case LOCATION_PERMISSION_REQUEST_CODE :{
+                if(grantResults.length > 0){
+                    for(int i = 0; i < grantResults.length; i++) {
+                        mLocationPermissionsGranted = false;
+                        Log.d(TAG, "onRequestPermissionsResults: permissions failed");
+                        return;
+                    }
+                    Log.d(TAG, "onRequestPermissionsResults: permissions granted");
+                    mLocationPermissionsGranted = true;
+                    //initialize map
+                    initMap();
+
+                }
+            }
+        }
+    }
+
+
+    private String getUrl(double latitude , double longitude , String nearbyPlace)
     {
-        if(ContextCompat.checkSelfPermission(this.getActivity(),Manifest.permission.ACCESS_FINE_LOCATION)  != PackageManager.PERMISSION_GRANTED )
-        {
+        Bundle bundle = this.getArguments();
+        brand = bundle.getString("brand");
 
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this.getActivity(),Manifest.permission.ACCESS_FINE_LOCATION))
-            {
-                ActivityCompat.requestPermissions(this.getActivity(),new String[] {Manifest.permission.ACCESS_FINE_LOCATION },REQUEST_LOCATION_CODE);
-            }
-            else
-            {
-                ActivityCompat.requestPermissions(this.getActivity(),new String[] {Manifest.permission.ACCESS_FINE_LOCATION },REQUEST_LOCATION_CODE);
-            }
-            return false;
+        brand =  brand.replaceAll("\\s+","+");
 
-        }
-        else
-            return true;
-    }
+        StringBuilder googlePlaceUrl = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        googlePlaceUrl.append("location="+latitude+","+longitude);
+        googlePlaceUrl.append("&radius="+PROXIMITY_RADIUS);
+        googlePlaceUrl.append("&type="+"store");
+        googlePlaceUrl.append("&keyword="+ brand);
+        googlePlaceUrl.append("&key="+"AIzaSyAQ0tHxeLZE3pElkuH5xiTl12lrBhMoSlw");
 
-    @Override
-    public void onConnectionSuspended(int i) {
+        Log.d("MapsActivity", "url = "+googlePlaceUrl.toString());
 
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+        return googlePlaceUrl.toString();
     }
 
 }
